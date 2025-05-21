e74de02342742e71e391ec7b98ec46aa14d4e6e5
João Diniz <joaodiniz.oliv.almeida+gitlabb@gmail.com>
2025-05-21 20:49:09 -0300

    Melhoria na organização da interface da gincana e do código

--- exercicio2/js/main.js
+++ exercicio2/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const buttonEnviar = document.getElementById('buttonEnviar');
 
     const randomGenerate = () => {
-        return Math.floor(Math.random() * 50);
+        return Math.floor(Math.random() * 50) + 1;
     }
     let randomNumber = randomGenerate();
 
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let tentativasMsg = document.getElementById('tentativas');
     let tentativas = 0;
     let numeros = [];
+    let running = true;
     console.log(`Apenas para testes o número aleatório é: ${randomNumber}`);
 
     const menorOuMaior = (number, random) => {
@@ -17,31 +18,36 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const verificarNum = () => {
-        entrada = parseInt(document.getElementById('inputNumero').value);
-        console.log(`${entrada} - random: ${randomNumber}`);
-        if (entrada == randomNumber) {
-            console.log("CERTO")
-            msg.textContent = `Você acertou! O número era: ${randomNumber}`;
-            tentativasMsg.textContent = `Suas tentativas foram: ${numeros}`;
+        if(running){
+
+            entrada = parseInt(document.getElementById('inputNumero').value);
+            console.log(`${entrada} - random: ${randomNumber}`);
+            if (entrada == randomNumber) {
+                msg.textContent = `Você acertou! O número era: ${randomNumber}`;
+                tentativasMsg.textContent = `Suas tentativas foram: ${numeros}`;
+                running = false
+            } else {
+                verificarTentativas(entrada);
+            }
         } else {
-            verificarTentativas(entrada);
+            msg.textContent = `Você já acertou, o número era: ${randomNumber}`;
         }
     }
 
     const verificarTentativas = (entrada) => {
-        if (entrada >= 0 && entrada <= 50) {
+        if (entrada >= 1 && entrada <= 50) {
             if (tentativas <= 9) {
                 numeros.push(` ${entrada}`);
                 msg.textContent = menorOuMaior(entrada, randomNumber);
                 tentativas++;
-                tentativasMsg.textContent = `Total de tentativas: ${tentativas}`;
+                tentativasMsg.textContent = `Total de tentativas: ${tentativas}\nTentativas restantes: ${10 - tentativas}`;
             } else {
                 msg.textContent = `Você atingiu o máximo de tentativas, o número correto era: ${randomNumber}`;
                 tentativasMsg.textContent = `Suas tentativas foram: ${numeros}`;
             }
         } else {
-            tentativasMsg.textContent = `É necessário que o número seja de 0 à 50.`;
-            msg.textContent = `Tentativa não contabilizada!`;
+            tentativasMsg.textContent = `Tentativa não contabilizada!`;
+            msg.textContent = `É necessário que o número seja de 0 à 50.`;
         }
     }
 
